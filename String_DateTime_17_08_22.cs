b2074fe16ebd2296c130d7510cd0d4618099e6e3
ANANTH B <90272112+ANANTH4004@users.noreply.github.com>
2022-08-17 10:53:11 +0530

    String DateTime 17-08-2022

---
+++ String_DateTime_17_08_22.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Day2
+{
+    internal class String_DateTime
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            DateTime DOB;
+            Console.WriteLine("Enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter your Date of Birth");
+            DOB = Convert.ToDateTime(Console.ReadLine());
+            age = Calculation(DOB);
+            Console.WriteLine("Name : "+name +"\n Date of Birth : "+ DOB + "\n Age : "+ age);
+            
+
+        }
+        static int Calculation(DateTime date)
+        {
+            int bYear = date.Year;
+            int cYear = DateTime.Now.Year;
+            int age = cYear - bYear;
+            return age;
+        }
+    }
+}
