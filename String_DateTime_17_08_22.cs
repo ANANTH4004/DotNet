c5a458f7acd62c11d449eb6d98f65e3c0f7b13c7
ANANTH B <90272112+ANANTH4004@users.noreply.github.com>
2022-08-17 12:59:53 +0530

    17_08_2022

--- String_DateTime_17_08_22.cs
+++ String_DateTime_17_08_22.cs
@@ -2,19 +2,23 @@
 
 namespace Day2
 {
-    internal class String_DateTime
+    internal class String_DateTime_17_08_22
     {
         static void Main(string[] args)
         {
-            string name;
-            int age;
-            DateTime DOB;
-            Console.WriteLine("Enter your name");
-            name = Console.ReadLine();
-            Console.WriteLine("Enter your Date of Birth");
-            DOB = Convert.ToDateTime(Console.ReadLine());
-            age = Calculation(DOB);
-            Console.WriteLine("Name : "+name +"\n Date of Birth : "+ DOB + "\n Age : "+ age);
+            //string name;
+            //int age;
+            //DateTime DOB;
+            //Console.WriteLine("Enter your name");
+            //name = Console.ReadLine();
+            //Console.WriteLine("Enter your Date of Birth");
+            //DOB = Convert.ToDateTime(Console.ReadLine());
+            //age = Calculation(DOB);
+            //Console.WriteLine("Name : "+name +"\nDate of Birth : "+ DOB + "\nAge : "+ age);
+            ArrayDemo demo1 = new ArrayDemo();
+            demo1.Loop1();
+            demo1.Sort1();
+            //demo1.Loop1();
             
 
         }
