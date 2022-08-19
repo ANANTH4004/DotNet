015493f2d2f44e7cbfdd5957207e12724a2d3c63
ANANTH4004 <balamuruganananth@gmail.com>
2022-08-19 19:37:44 +0530

    Assignment

---
+++ 19-08-2022/LoopIteration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_08_2022_Assignment
+{
+    internal class LoopIteration
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1, j = 25; i <= j; i++, j--)
+            {
+                Console.WriteLine("i ={0} and j = {1}",i,j);
+            }
+        }
+    }
+}
---
+++ 19-08-2022/MultipleIf.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace _19_08_2022_Assignment
+{
+    internal class MultipleIf
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a Number:");
+            n = Int32.Parse(Console.ReadLine());
+            if (n < 100)
+            {
+                if (n == 0)
+                {
+                    Console.WriteLine("The number is Zero.");
+                }
+                else if(n%2==0)
+                {
+                    if(n%10==0)
+                        Console.WriteLine(" Even Number and Divide by 10");
+                    Console.WriteLine(" Even Number");
+                }
+                else if (n % 2 == 1)
+                {
+                    Console.WriteLine("Odd Number");
+                }
+            }
+            else
+            {
+                if (n % 10 == 0)
+                {
+                    Console.WriteLine(" large Number and  devide by 10");
+                }
+                else
+                {
+                    Console.WriteLine(" large Number.");
+                }
+            }
+        }
+    }
+}
