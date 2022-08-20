9ba66a7852e9aec6965ce4b064166fe3af821896
ANANTH4004 <balamuruganananth@gmail.com>
2022-08-20 09:18:49 +0530

    Assignment

---
+++ 18-08-2022/Age.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_08_2022
+{
+    internal class Age
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter you Age : ");
+            int i = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("You look older than {0}",i);
+        }
+    }
+}
---
+++ 18-08-2022/PositiveCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_08_2022
+{
+    internal class PositiveCheck
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first Number:");
+            int i = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second Number:");
+            int j = Int32.Parse(Console.ReadLine());
+            if( i>=0 && j>=0)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+        }
+    }
+}
