cc196e1207e21416abba0b5e07c10d007ce868ef
ANANTH4004 <balamuruganananth@gmail.com>
2022-08-22 20:29:05 +0530

    recursion method

---
+++ 22-08-2022/Assignment14.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_08_22
+{
+    internal class Assignment14
+    {
+        static void Main(string[] args)
+        {
+
+            int[] s = { 1, 2, 3 };
+            int m = s.Length;
+            Console.WriteLine("Total combination {0}",Calculate(s, m, 10));
+
+        }
+        static int Calculate(int[] arr, int size, int total)
+        {
+
+            if (total == 0)
+                return 1;
+
+
+            if (total < 0)
+                return 0;
+
+
+            if (size <= 0)
+                return 0;
+
+
+            return Calculate(arr, size - 1, total) +
+                Calculate(arr, size, total - arr[size - 1]);
+        }
+    }
+}
