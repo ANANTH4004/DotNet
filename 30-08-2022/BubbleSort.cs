84fc688d38b15c6e58c1fbd8cebd3f429da40bfe
ANANTH4004 <balamuruganananth@gmail.com>
2022-08-30 11:07:28 +0530

    Bubble Sort and Selection Sort

---
+++ 30-08-2022/BubbleSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class BubbleSort
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            int temp;
+            Console.WriteLine("Enter 5 Number");
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("========= Before Sort ===========");
+            Print(a);
+            for (int i = 0; i < n-1; i++)
+            {
+                for (int j  = 0; j < n-1-i; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("========= After Sort ===========");
+            Print(a);
+        }
+        static void Print(int[] arr)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(arr[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 30-08-2022/SelectionSort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class SelectionSort
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            int min ,temp;
+            Console.WriteLine("Enter 5 Number");
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("========= Before Sort ===========");
+            Print(a);
+            for(int i = 0; i < a.Length - 1; i++)
+            {
+                min = i;
+                for (int j = i+1; j < a.Length; j++)
+                {
+                    if (a[j] < a[min])
+                    {
+                        min = j;
+                    }
+                }
+                if(min != i)
+                {
+                    temp = a[i];
+                    a[i] = a[min];
+                    a[min] = temp;
+                }
+            }
+            Console.WriteLine("========= After Sort ===========");
+            Print(a);
+        }
+        static void Print(int[] arr)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(arr[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
