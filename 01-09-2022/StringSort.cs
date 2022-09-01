fb0ea292230848ca92735ba7634a33eff6202f85
ANANTH4004 <balamuruganananth@gmail.com>
2022-09-01 22:42:02 +0530

    Custom String Sort

---
+++ 01-09-2022/StringSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class StringSort
+    {
+        static void Main(string[] args)
+        {
+            List<string> sl = new List<string>() { "1Anand", "snithya", "2varun" , "Anand" , "varun" , "xnithya"};
+            Print(sl);
+            Console.WriteLine("======== Sorted List=======");
+            sl.Sort((a,b)=> a[a.Length-1].CompareTo(b[b.Length-1]));
+            Print(sl);
+        }
+        static void Print(List<string> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("=============================");
+        }
+    }
+}
