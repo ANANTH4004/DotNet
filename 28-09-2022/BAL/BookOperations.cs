953125e7931f48843c18958f8c218bd652817352
ANANTH4004 <balamuruganananth@gmail.com>
2022-09-28 21:52:39 +0530

    Entity FrameWork

---
+++ 28-09-2022/BAL/BookOperations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using System.Data.Entity;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+
+namespace BAL
+{
+    public class BookOperations
+    {
+        public void InsertBook(BookBal b)
+        {
+            ShoppingEntities context = new ShoppingEntities();
+            Book bookdal = new Book();
+            bookdal.Book_No = b.Book_No;
+            bookdal.Book_Name = b.Book_Name;
+            bookdal.Author = b.Author;
+            bookdal.Cost = b.Cost;
+            bookdal.Category = b.Category;
+            context.Books.Add(bookdal);
+            context.SaveChanges();
+        }
+        public void UpdateBook(BookBal b)
+        {
+            ShoppingEntities context = new ShoppingEntities();
+            // context.s(b.Book_No, b.Book_Name, b.Author, b.Cost, b.Category);
+            context.sp_UpdateBook(b.Book_No, b.Book_Name, b.Author, b.Cost, b.Category);
+        }
+        public int Count()
+        {
+            ShoppingEntities context = new ShoppingEntities();
+           // _db = new DbEntities();
+            int id =  context.sp_BookCountFinal();
+            return id;
+        }
+        public List<BookBal> GetAllBooks()
+        {
+            ShoppingEntities context = new ShoppingEntities();
+            var ans = context.GetAllBook();
+            List<BookBal> book = new List<BookBal>();
+            foreach (var item in ans)
+            {
+                book.Add(new BookBal { Book_No = item.Book_No, Book_Name = item.Book_Name, Author = item.Author, Cost = item.Cost, Category = item.Category });
+            }
+            return book;
+        }
+
+    }
+
+   
+}
---
+++ 28-09-2022/EFAssignment/Program.cs
@@ -0,0 +1,66 @@
+﻿using BAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("1.Insert Book 2.Update Book 3.Book Count 4.Get All Books");
+            int ch = Int32.Parse(Console.ReadLine());
+            BookOperations b = new BookOperations();
+            BookBal book = new BookBal();
+            switch (ch)
+            {
+                case 1:
+                    
+                    Console.WriteLine("Enter Book Number :");
+                    book.Book_No = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Book Name :");
+                    book.Book_Name = Console.ReadLine();
+                    Console.WriteLine("Enter Author ;");
+                    book.Author = Console.ReadLine();
+                    Console.WriteLine("Enter Cost of the Book");
+                    book.Cost = decimal.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Category");
+                    book.Category = Console.ReadLine();
+                    b.InsertBook(book);
+                    break;
+                case 2:
+                    //BookBal book = new BookBal();
+                    Console.WriteLine("Enter Book Number :");
+                    book.Book_No = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Book Name :");
+                    book.Book_Name = Console.ReadLine();
+                    Console.WriteLine("Enter Author ;");
+                    book.Author = Console.ReadLine();
+                    Console.WriteLine("Enter Cost of the Book");
+                    book.Cost = decimal.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Category");
+                    book.Category = Console.ReadLine();
+                    b.UpdateBook(book);
+                    break;
+                case 3:
+                    int id = b.Count();
+                    Console.WriteLine("Book Count  : " + id );
+                    break;
+                case 4:
+                    List<BookBal> bookList = new List<BookBal>();
+                    bookList = b.GetAllBooks();
+                    foreach (var item in bookList)
+                    {
+                        Console.WriteLine(item.Book_No + " " + item.Book_Name + " " + item.Author + " " + item.Cost);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
