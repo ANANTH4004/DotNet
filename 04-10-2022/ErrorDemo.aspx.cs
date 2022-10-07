a14b959b407dcb251c8473cb478a19218576076f
ANANTH4004 <balamuruganananth@gmail.com>
2022-10-07 09:11:48 +0530

    Error Pages

---
+++ 04-10-2022/ErrorDemo.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace webapp1
+{
+    public partial class ErrorDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Page_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+               Response.Redirect("~/ErrorPage.aspx");
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            DataSet ds = new DataSet();
+            SqlConnection sql = new SqlConnection(ConfigurationManager.ConnectionStrings["LoginCnStr"].ConnectionString);
+            SqlDataAdapter da = new SqlDataAdapter("Select * from IssueTable", sql);
+          
+            da.Fill(ds, "IssueTable");
+        }
+    }
+}
